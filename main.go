fefd54b1fdf013409e5bea87cb4971a9e443db3a
LorenzoChukwuebuka <lawrenceobi2@gmail.com>
2023-05-10 16:43:16 +0100

    added some basic files

--- main.go
+++ main.go
@@ -1,48 +1,21 @@
 package main
 
 import (
-	"fmt"
-	"math"
+	"github.com/gofiber/fiber/v2"
+	"log"
 )
 
-type Shape interface {
-	Area() float64
-	Perimeter() float64
+func welcome(c *fiber.Ctx) error {
+	return c.SendString("welcome to api")
 }
 
-type Circle struct {
-	radius float64
-}
-
-type Rectangle struct {
-	width, height float64
-}
-
-func (r Rectangle) Area() float64 {
-	return r.width * r.height
-}
-
-func (r Rectangle) Perimeter() float64 {
-	return 2 * (r.width + r.height)
-}
-
-func (c Circle) Area() float64 {
-	return math.Pi * c.radius * c.radius
-}
-
-func (c Circle) Perimeter() float64 {
-	return 2 * math.Pi * c.radius
-}
-
-func PrintShapeInfo(s Shape) {
-	fmt.Printf("Area: %f, Perimeter: %f\n", s.Area(), s.Perimeter())
+func setupRoutes(app *fiber.App) {
+	app.Get("/api", welcome)
 }
 
 func main() {
-	r := Rectangle{width: 2, height: 3}
-	c := Circle{radius: 1}
 
-	PrintShapeInfo(r) // Output: Area: 6.000000, Perimeter: 10.000000
-	PrintShapeInfo(c) // Output: Area: 3.141593, Perimeter: 6.283185
-
+	app := fiber.New()
+	setupRoutes(app)
+	log.Fatal(app.Listen(":3000"))
 }
