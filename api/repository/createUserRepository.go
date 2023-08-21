61e2947eee7d40a72ad728ba3692d8785e000b12
LorenzoChukwuebuka <lawrenceobi2@gmail.com>
2023-08-21 18:00:57 +0100

    finished user signup

--- api/repository/createUserRepository.go
+++ api/repository/createUserRepository.go
@@ -1,8 +1,25 @@
 package repository
 
-import "email-marketing-service/api/model"
+import (
+	"email-marketing-service/api/database"
+	"email-marketing-service/api/model"
+)
 
-func CreateUser(d *model.User) *model.User {
+func CreateUser(d *model.User) (*model.User, error) {
+	// Initialize the database connection
+	db, err := database.InitDB()
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
+	query := "INSERT INTO users (uuid,firstname,middlename,lastname,username, email,password,created_at) VALUES ($1,$2,$3,$4,$5,$6,$7,$8)"
+	_, err = db.Exec(query, d.UUID, d.FirstName, d.MiddleName, d.LastName, d.UserName, d.Email, d.Password, d.CreatedAt)
+	if err != nil {
+		return nil, err
+	}
+	return d, nil
+}
 
-	return d
+func CheckIfEmailAlreadyExists(d *model.User) {
+
 }
