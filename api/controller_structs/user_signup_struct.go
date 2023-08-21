3ee254825b7414b0db8dd35b1aefa6f1c7193e40
LorenzoChukwuebuka <lawrenceobi2@gmail.com>
2023-08-21 15:36:34 +0100

    completed the user reg function

--- api/controller_structs/user_signup_struct.go
+++
@@ -1,8 +0,0 @@
-package controllerstructs
-
-type UserSignUp struct {
-	FirstName  string `json:"firstname" validate:"required"`
-	MiddleName string `json:"middlename"`
-	LastName   string `json:"lastname" validate:"required"`
-	Email      string `json:"email" validate:"required"`
-}
---
+++ api/repository/createUserRepository.go
@@ -0,0 +1,8 @@
+package repository
+
+import "email-marketing-service/api/model"
+
+func CreateUser(d *model.User) *model.User {
+
+	return d
+}
