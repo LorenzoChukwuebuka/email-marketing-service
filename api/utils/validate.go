f20675aac77778d3f7b7f126effef4bed36b9400
LorenzoChukwuebuka <lawrenceobi2@gmail.com>
2023-08-23 15:46:42 +0100

    error issue in the user service

--- api/utils/validate.go
+++ api/utils/validate.go
@@ -3,26 +3,50 @@ package utils
 import (
 	"fmt"
 	"github.com/go-playground/validator/v10"
+	"strings"
 )
 
+// func ValidateData(v interface{}) map[string]string {
+// 	validate := validator.New()
+
+// 	err := validate.Struct(v)
+
+// 	if err != nil {
+// 		validationErrors, ok := err.(validator.ValidationErrors)
+// 		if !ok {
+// 			// Handle unexpected validation error type
+// 			return map[string]string{"error": err.Error()}
+// 		}
+// 		// Construct a response with validation errors
+// 		errorMap := make(map[string]string)
+// 		for _, e := range validationErrors {
+// 			errorMap[e.Field()] = e.Tag()
+// 		}
+// 		return errorMap
+// 	}
+
+// 	return nil // No validation errors
+// }
+
 func ValidateData(v interface{}) error {
-	var validate = validator.New()
+	validate := validator.New()
 
 	err := validate.Struct(v)
 
 	if err != nil {
-		validationErrors := err.(validator.ValidationErrors)
-		// Construct a response with validation errors
-		errorMap := make(map[string]string)
-		for _, e := range validationErrors {
-			errorMap[e.Field()] = e.Tag()
+		validationErrors, ok := err.(validator.ValidationErrors)
+		if !ok {
+			// Handle unexpected validation error type
+			return fmt.Errorf("unexpected validation error: %v", err)
 		}
-		errorResponse := map[string]interface{}{"errors": errorMap}
 
-		return fmt.Errorf("validation errors: %v", errorResponse)
-
+		// Construct an error message with validation errors
+		var errorMsgs []string
+		for _, e := range validationErrors {
+			errorMsgs = append(errorMsgs, fmt.Sprintf("%s: %s", e.Field(), e.Tag()))
+		}
+		return fmt.Errorf("validation errors: %s", strings.Join(errorMsgs, "; "))
 	}
 
-	return nil
-
+	return nil // No validation errors
 }
