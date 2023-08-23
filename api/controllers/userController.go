bfcfe9cacbeaf846301c65b963d26408ee1c5763
LorenzoChukwuebuka <lawrenceobi2@gmail.com>
2023-08-23 21:19:22 +0100

    kindly help me out with the naming conventions

--- api/controllers/userController.go
+++ api/controllers/userController.go
@@ -10,13 +10,15 @@ import (
 	"github.com/golang-jwt/jwt"
 )
 
+type UserController struct{}
+
 //instantiate otp service
 
 func NewUserService() *services.UserService {
 	return &services.UserService{}
 }
 
-func Welcome(w http.ResponseWriter, r *http.Request) {
+func (c *UserController) Welcome(w http.ResponseWriter, r *http.Request) {
 	claims, ok := r.Context().Value("jwtclaims").(jwt.MapClaims)
 	if !ok {
 		http.Error(w, "Invalid claims", http.StatusInternalServerError)
@@ -32,7 +34,7 @@ func Welcome(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello world")
 }
 
-func RegisterUser(w http.ResponseWriter, r *http.Request) {
+func (c *UserController) RegisterUser(w http.ResponseWriter, r *http.Request) {
 	var reqdata *model.User
 
 	utils.DecodeRequestBody(r, &reqdata)
@@ -47,7 +49,7 @@ func RegisterUser(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func VerifyUser(w http.ResponseWriter, r *http.Request) {
+func (c *UserController) VerifyUser(w http.ResponseWriter, r *http.Request) {
 	var reqdata *model.OTP
 
 	utils.DecodeRequestBody(r, &reqdata)
@@ -61,7 +63,7 @@ func VerifyUser(w http.ResponseWriter, r *http.Request) {
 	utils.SuccessResponse(w, 200, "User has been successfully verifed")
 }
 
-func Login(w http.ResponseWriter, r *http.Request) {
+func (c *UserController) Login(w http.ResponseWriter, r *http.Request) {
 	var reqdata *model.LoginModel
 
 	utils.DecodeRequestBody(r, &reqdata)
@@ -76,7 +78,7 @@ func Login(w http.ResponseWriter, r *http.Request) {
 	utils.SuccessResponse(w, 200, result)
 }
 
-func ForgetPassword(w http.ResponseWriter, r *http.Request) {
+func (c *UserController) ForgetPassword(w http.ResponseWriter, r *http.Request) {
 	var reqdata *model.ForgetPassword
 
 	utils.DecodeRequestBody(r, &reqdata)
@@ -91,7 +93,7 @@ func ForgetPassword(w http.ResponseWriter, r *http.Request) {
 	utils.SuccessResponse(w, 200, "email sent successfully")
 }
 
-func ResetPassword(w http.ResponseWriter, r *http.Request) {
+func (c *UserController) ResetPassword(w http.ResponseWriter, r *http.Request) {
 	var reqdata *model.ResetPassword
 
 	utils.DecodeRequestBody(r, &reqdata)
