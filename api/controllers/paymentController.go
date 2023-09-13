57d1dd301bae16b796cf4eb01a0978891e74bc8a
LorenzoChukwuebuka <lawrenceobi2@gmail.com>
2023-09-13 10:51:04 +0100

    added payment controller and service

--- api/controllers/paymentController.go
+++ api/controllers/paymentController.go
@@ -1,1 +1,24 @@
 package controllers
+
+import (
+	"email-marketing-service/api/services"
+	"net/http"
+)
+
+type PaymentController struct {
+	PaymentService *services.PaymentService
+}
+
+func NewPaymentController(paymentService *services.PaymentService) *PaymentController {
+	return &PaymentController{
+		PaymentService: paymentService,
+	}
+}
+
+func (c *PaymentController) InitializePayment(w http.ResponseWriter, r *http.Request) {}
+
+func (c *PaymentController) ConfirmPayment(w http.ResponseWriter, r *http.Request) {}
+
+func (c *PaymentController) GetAllPaymentsForAUser(w http.ResponseWriter, r *http.Request) {}
+
+func (c *PaymentController) GetSinglePaymentForAUser(w http.ResponseWriter, r *http.Request) {}
--- api/services/paymentService.go
+++ api/services/paymentService.go
@@ -1,1 +1,32 @@
 package services
+
+import (
+	"email-marketing-service/api/model"
+	"email-marketing-service/api/repository"
+)
+
+type PaymentService struct {
+	PaymentRepo *repository.PaymentRepository
+}
+
+func NewPaymentService(paymentRepo *repository.PaymentRepository) *PaymentService {
+	return &PaymentService{
+		PaymentRepo: paymentRepo,
+	}
+}
+
+func (s *PaymentService) InitializePayment(d interface{}) (map[string]interface{}, error) {
+	return nil, nil
+}
+
+func (s *PaymentService) ConfirmPayment(d *model.PaymentModel) error {
+	return nil
+}
+
+func (s *PaymentService) GetAllPaymentsForAUser(userId int) ([]model.PaymentResponse, error) {
+	return nil, nil
+}
+
+func (s *PaymentService) GetSinglePaymentForAUser(userId int, paymentId int) (*model.PaymentResponse, error) {
+	return nil, nil
+}
