dbfbebe10dc90b0c172611b47201dfb4fc69bab3
LorenzoChukwuebuka <lawrenceobi2@gmail.com>
2023-09-14 15:26:10 +0100

    done with the payments

--- api/services/paymentService.go
+++ api/services/paymentService.go
@@ -7,6 +7,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/go-resty/resty/v2"
+	"net/http"
 	"os"
 	"strconv"
 	"strings"
@@ -14,12 +15,14 @@ import (
 )
 
 type PaymentService struct {
-	PaymentRepo *repository.PaymentRepository
+	PaymentRepo     *repository.PaymentRepository
+	SubscriptionSvc *SubscriptionService
 }
 
-func NewPaymentService(paymentRepo *repository.PaymentRepository) *PaymentService {
+func NewPaymentService(paymentRepo *repository.PaymentRepository, subscriptionSvc *SubscriptionService) *PaymentService {
 	return &PaymentService{
-		PaymentRepo: paymentRepo,
+		PaymentRepo:     paymentRepo,
+		SubscriptionSvc: subscriptionSvc,
 	}
 }
 
@@ -68,13 +71,11 @@ func (s *PaymentService) InitializePayment(d *model.PaymentModel) (map[string]in
 	return response, nil
 }
 
-func (s *PaymentService) ConfirmPayment(reference string) (map[string]interface{}, error) {
+func (s *PaymentService) ConfirmPayment(reference string) (string, error) {
 	utils.LoadEnv()
 
-	var (
-		key      = os.Getenv("PAYSTACK_KEY")
-		api_base = os.Getenv("PAYSTACK_BASE_URL")
-	)
+	key := os.Getenv("PAYSTACK_KEY")
+	api_base := os.Getenv("PAYSTACK_BASE_URL")
 
 	url := fmt.Sprintf(api_base+"transaction/verify/%s", reference)
 
@@ -86,87 +87,56 @@ func (s *PaymentService) ConfirmPayment(reference string) (map[string]interface{
 		Get(url)
 
 	if err != nil {
-		return nil, fmt.Errorf("error sending request: %s", err)
+		return "", fmt.Errorf("error sending request: %s", err)
 	}
 
-	if resp.StatusCode() != 200 {
-		return nil, fmt.Errorf("unexpected status code: %d", resp.StatusCode())
+	if resp.StatusCode() != http.StatusOK {
+		return "", fmt.Errorf("unexpected status code: %d", resp.StatusCode())
 	}
 
 	var result map[string]interface{}
 	if err := json.Unmarshal(resp.Body(), &result); err != nil {
-		return nil, fmt.Errorf("error decoding response body: %s", err)
+		return "", fmt.Errorf("error decoding response body: %s", err)
 	}
 
 	if status, ok := result["status"].(bool); ok && !status {
-		return nil, fmt.Errorf("transaction failed")
+		return "", fmt.Errorf("transaction failed")
 	}
 
-	data := result["data"].(map[string]interface{})
+	data, ok := result["data"].(map[string]interface{})
+	if !ok {
+		return "", fmt.Errorf("invalid response format")
+	}
 
 	amount := data["amount"].(float64) / 100
-	planIDStr := data["metadata"].(map[string]interface{})["plan_id"].(string)
-	userIDStr := data["metadata"].(map[string]interface{})["user_id"].(string)
-	duration := data["metadata"].(map[string]interface{})["duration"].(string)
-	email := data["customer"].(map[string]interface{})["email"].(string)
-
-	//convert planId and userId to int
+	planIDStr, _ := data["metadata"].(map[string]interface{})["plan_id"].(string)
+	userIDStr, _ := data["metadata"].(map[string]interface{})["user_id"].(string)
+	duration, _ := data["metadata"].(map[string]interface{})["duration"].(string)
+	email, _ := data["customer"].(map[string]interface{})["email"].(string)
 
 	planID, err := strconv.Atoi(planIDStr)
 	if err != nil {
-		return nil, fmt.Errorf("error converting planID to int: %s", err)
+		return "", fmt.Errorf("error converting planID to int: %s", err)
 	}
 
 	userID, err := strconv.Atoi(userIDStr)
 	if err != nil {
-		return nil, fmt.Errorf("error converting userID to int: %s", err)
+		return "", fmt.Errorf("error converting userID to int: %s", err)
 	}
 
-	//split the duration parts
-	//perform a logic with it to get the exipry date for the payment made
-	parts := strings.Split(duration, " ")
-
-	num := parts[0]
-	unit := parts[1]
-
-	var expiryDate time.Time
-
-	switch unit {
-	case "week":
-		numWeeks, err := strconv.Atoi(num)
-		if err != nil {
-			return nil, fmt.Errorf("error converting num to int: %s", err)
-		}
-		expiryDate = time.Now().AddDate(0, 0, numWeeks*7)
-	case "day":
-		numDays, err := strconv.Atoi(num)
-		if err != nil {
-			return nil, fmt.Errorf("error converting num to int: %s", err)
-		}
-		expiryDate = time.Now().AddDate(0, 0, numDays)
-	case "year":
-		numYears, err := strconv.Atoi(num)
-		if err != nil {
-			return nil, fmt.Errorf("error converting num to int: %s", err)
-		}
-		expiryDate = time.Now().AddDate(numYears, 0, 0)
-	case "month":
-		numMonths, err := strconv.Atoi(num)
-		if err != nil {
-			return nil, fmt.Errorf("error converting num to int: %s", err)
-		}
-		expiryDate = time.Now().AddDate(0, numMonths, 0)
-	default:
-		return nil, fmt.Errorf("unsupported unit: %s", unit)
-	}
+	// Parse duration and calculate expiry date
 
 	tx, err := s.PaymentRepo.DB.Begin()
-
 	if err != nil {
-		return nil, err
+		return "", fmt.Errorf("error starting transaction: %s", err)
 	}
 
-	defer tx.Rollback()
+	defer func() {
+		if r := recover(); r != nil {
+			// An error occurred, rollback the transaction
+			tx.Rollback()
+		}
+	}()
 
 	payment := &model.PaymentModel{
 		AmountPaid: float32(amount),
@@ -174,15 +144,15 @@ func (s *PaymentService) ConfirmPayment(reference string) (map[string]interface{
 		UserId:     userID,
 		Duration:   duration,
 		Reference:  reference,
-		ExpiryDate: expiryDate,
+		ExpiryDate: calculateExpiryDate(duration),
 		Email:      email,
 		CreatedAt:  time.Now(),
+		Status:     "active",
 	}
 
 	paymentRepo, err := s.PaymentRepo.CreatePayment(payment)
-
 	if err != nil {
-		return nil, err
+		return "", fmt.Errorf("error creating payment: %s", err)
 	}
 
 	subscription := &model.SubscriptionModel{
@@ -190,19 +160,41 @@ func (s *PaymentService) ConfirmPayment(reference string) (map[string]interface{
 		PlanId:    planID,
 		PaymentId: paymentRepo.Id,
 		StartDate: time.Now(),
-		EndDate:   expiryDate,
+		EndDate:   calculateExpiryDate(duration),
 		Expired:   false,
 		CreatedAt: time.Now(),
 	}
 
-	fmt.Print(subscription)
+	_, err = s.SubscriptionSvc.CreateSubscription(subscription)
+	if err != nil {
+		return "", fmt.Errorf("error creating subscription: %s", err)
+	}
 
 	err = tx.Commit()
 	if err != nil {
-		return nil, fmt.Errorf("error committing transaction: %s", err)
+		return "", fmt.Errorf("error committing transaction: %s", err)
 	}
 
-	return result, nil
+	return "payment verified successfully", nil
+}
+
+func calculateExpiryDate(duration string) time.Time {
+	parts := strings.Split(duration, " ")
+	num, _ := strconv.Atoi(parts[0])
+	unit := parts[1]
+
+	switch unit {
+	case "week":
+		return time.Now().AddDate(0, 0, num*7)
+	case "day":
+		return time.Now().AddDate(0, 0, num)
+	case "year":
+		return time.Now().AddDate(num, 0, 0)
+	case "month":
+		return time.Now().AddDate(0, num, 0)
+	default:
+		return time.Now()
+	}
 }
 
 func (s *PaymentService) GetAllPaymentsForAUser(userId int) ([]model.PaymentResponse, error) {
--- api/services/subscriptionService.go
+++ api/services/subscriptionService.go
@@ -1,4 +1,22 @@
 package services
 
+import (
+	"email-marketing-service/api/model"
+	"email-marketing-service/api/repository"
+)
+
 type SubscriptionService struct {
+	SubscriptionRepo *repository.SubscriptionRepository
+}
+
+func NewSubscriptionService(subscriptionRepo *repository.SubscriptionRepository) *SubscriptionService {
+	return &SubscriptionService{SubscriptionRepo: subscriptionRepo}
+}
+
+func (s *SubscriptionService) CreateSubscription(d *model.SubscriptionModel) (*model.SubscriptionModel, error) {
+
+	if err := s.SubscriptionRepo.CreateSubscription(d); err != nil {
+		return nil, err
+	}
+	return d, nil
 }
