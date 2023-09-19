46e9575eae6471d0fe9b54456edb53fec7a70b6a
LorenzoChukwuebuka <lawrenceobi2@gmail.com>
2023-09-19 14:32:44 +0100

    added migration files

--- migrations/20230919131755_create_plan_table.sql
+++ migrations/20230919131755_create_plan_table.sql
@@ -2,7 +2,19 @@
 -- +goose StatementBegin
 SELECT 'up SQL query';
 -- +goose StatementEnd
-
+CREATE TABLE IF NOT EXISTS public.plans
+(
+    id integer NOT NULL DEFAULT nextval('plans_id_seq'::regclass),
+    planname character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    duration character varying(50) COLLATE pg_catalog."default" NOT NULL,
+    price real NOT NULL,
+    details text COLLATE pg_catalog."default" NOT NULL,
+    status character varying(50) COLLATE pg_catalog."default",
+    created_at timestamp with time zone NOT NULL,
+    updated_at timestamp with time zone,
+    deleted_at timestamp with time zone,
+    CONSTRAINT plans_pkey PRIMARY KEY (id)
+)
 -- +goose Down
 -- +goose StatementBegin
 SELECT 'down SQL query';
