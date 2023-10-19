1557938b45b6423bc1487afabbac7e0e8e0b46c4
Michael Okolo <okolomikel2009@gmail.com>
2023-10-20 00:35:57 +1100

    Provided an example of Payment Service Factory

--- api/services/TransactionFuncService.go
+++ api/services/TransactionFuncService.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"fmt"
+
 	"email-marketing-service/api/model"
 	paymentmethods "email-marketing-service/api/services/paymentMethodFactory"
 )
@@ -10,11 +11,10 @@ type Transaction struct {
 	selectedPaymentMethod paymentmethods.PaymentInterface
 }
 
-
-
 func (c *Transaction) ChoosePaymentMethod(paymentType string) error {
 	switch paymentType {
 	case "FlutterWave":
+		// return FlutterWavePaymentService
 		c.selectedPaymentMethod = &paymentmethods.FlutterwavePaymentProcessor{}
 	case "Paystack":
 		c.selectedPaymentMethod = &paymentmethods.PaystackPaymentProcessor{}
@@ -25,7 +25,32 @@ func (c *Transaction) ChoosePaymentMethod(paymentType string) error {
 	return nil
 }
 
-func (c *Transaction) OpenProcessPayment(d *model.InitPaymentModelData) (map[string]interface{}, error) {
+func (c *Transaction) OpenProcessPayment(
+	d *model.InitPaymentModelData,
+) (map[string]interface{}, error) {
 	return c.selectedPaymentMethod.InitializePaymentProcess(d)
+}
 
+func Factory(paymentMethod string) (paymentmethods.PaymentInterface, error) {
+	var pI paymentmethods.PaymentInterface
+	switch paymentMethod {
+	case "FlutterWave":
+		// Return a concrete implementation of Flutterwave
+		return nil, nil
+	case "Paystack":
+		pI = &paymentmethods.PaystackPaymentProcessor{}
+		return pI, nil
+	}
+
+	return nil, nil
+}
+
+func UsePayment() error {
+	paymentService, err := Factory("Paystack")
+	if err != nil {
+		return err
+	}
+
+	paymentService.Pay(20)
+	return nil
 }
--- api/services/paymentMethodFactory/paymentMethodsInterface.go
+++ api/services/paymentMethodFactory/paymentMethodsInterface.go
@@ -3,9 +3,37 @@ package paymentmethodFactory
 import "email-marketing-service/api/model"
 
 type PaymentInterface interface {
-	InitializePaymentProcess(d *model.InitPaymentModelData) (map[string]interface{},error)
-	Pay(amount float64)
+	InitializePaymentProcess(
+		d *model.InitPaymentModelData,
+	) (map[string]interface{}, error) // Not required.
+	Pay(amount float64) // Not required
 	Charge(amount float64)
 	Refund(amount float64)
-	Status() string
+	Status() string // You can call this PaymentStatus - could be better to return a bool
 }
+
+// type Enum struct {
+// 	data []EnumValues
+// }
+//
+// type EnumValues struct {
+// 	Key   string
+// 	Value string
+// }
+//
+// func NewEnum() *Enum {
+// 	return &Enum{}
+// }
+//
+// func (e *Enum) CreateEnums(enums map[string]string) {
+// 	for key, value := range enums {
+// 		d := EnumValues{Key: key, Value: value}
+// 		e.data = append(e.data, d)
+// 	}
+// }
+//
+// var p = map[string]string{
+// 	"S": "Successful",
+// 	"F": "Failed",
+// }
+// var PaymentStatus = NewEnum().CreateEnums(p)
