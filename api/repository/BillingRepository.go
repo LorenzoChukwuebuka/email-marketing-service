978b07a9d503f67b25f1a15981add6ab4b800aa5
LorenzoChukwuebuka <lawrenceobi2@gmail.com>
2023-10-24 14:48:32 +0100

    done

--- api/repository/BillingRepository.go
+++ api/repository/BillingRepository.go
@@ -3,6 +3,7 @@ package repository
 import (
 	"database/sql"
 	"email-marketing-service/api/model"
+	"time"
 )
 
 type BillingRepository struct {
@@ -13,8 +14,14 @@ func NewBillingRepository(db *sql.DB) *BillingRepository {
 	return &BillingRepository{DB: db}
 }
 
+var SetTime = func(field sql.NullTime, target *string) {
+	if field.Valid {
+		*target = field.Time.Format(time.RFC3339Nano)
+	}
+}
+
 func (r *BillingRepository) CreateBilling(d *model.BillingModel) (*model.BillingModel, error) {
-	query := "INSERT INTO payments (uuid,  user_id, amount_paid, plan_id, duration,  expiry_date, reference,paymentMethod,transaction_id, status, created_at) VALUES ( $1, $2, $3, $4, $5, $6, $7, $8,$9,$10,$11 ) RETURNING id"
+	query := "INSERT INTO billing (uuid,  user_id, amount_paid, plan_id, duration,  expiry_date, reference,paymentMethod,transaction_id, status, created_at) VALUES ( $1, $2, $3, $4, $5, $6, $7, $8,$9,$10,$11 ) RETURNING id"
 
 	stmt, err := r.DB.Prepare(query)
 
--- api/repository/paymentRepository.go
+++ api/repository/paymentRepository.go
@@ -1,353 +1,349 @@
 package repository
 
-import (
-	"database/sql"
-	"email-marketing-service/api/model"
-	"time"
-)
+// import (
+// 	"database/sql"
+// 	"email-marketing-service/api/model"
+// 	"time"
+// )
 
-type PaymentRepository struct {
-	DB *sql.DB
-}
+// type PaymentRepository struct {
+// 	DB *sql.DB
+// }
 
-var SetTime = func(field sql.NullTime, target *string) {
-	if field.Valid {
-		*target = field.Time.Format(time.RFC3339Nano)
-	}
-}
 
-func NewPaymentRepository(db *sql.DB) *PaymentRepository {
-	return &PaymentRepository{DB: db}
-}
 
-func (r *PaymentRepository) CreatePayment(d *model.PaymentModel) (*model.PaymentModel, error) {
-	query := "INSERT INTO payments (uuid,  user_id, amount_paid, plan_id, duration,  expiry_date, reference, status, created_at) VALUES ( $1, $2, $3, $4, $5, $6, $7, $8,$9 ) RETURNING id"
+// func NewPaymentRepository(db *sql.DB) *PaymentRepository {
+// 	return &PaymentRepository{DB: db}
+// }
 
-	stmt, err := r.DB.Prepare(query)
+// func (r *PaymentRepository) CreatePayment(d *model.BillingModel) (*model.BillingModel, error) {
+// 	query := "INSERT INTO payments (uuid,  user_id, amount_paid, plan_id, duration,  expiry_date, reference, status, created_at) VALUES ( $1, $2, $3, $4, $5, $6, $7, $8,$9 ) RETURNING id"
 
-	if err != nil {
-		return nil, err
-	}
+// 	stmt, err := r.DB.Prepare(query)
 
-	defer stmt.Close()
-	var insertedID int
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-	err = stmt.QueryRow(
-		d.UUID,
-		d.UserId,
-		d.AmountPaid,
-		d.PlanId,
-		d.Duration,
-		d.ExpiryDate,
-		d.Reference,
-		d.Status,
-		d.CreatedAt,
-	).Scan(&insertedID)
+// 	defer stmt.Close()
+// 	var insertedID int
 
-	d.Id = insertedID
-	if err != nil {
-		return nil, err
-	}
+// 	err = stmt.QueryRow(
+// 		d.UUID,
+// 		d.UserId,
+// 		d.AmountPaid,
+// 		d.PlanId,
+// 		d.Duration,
+// 		d.ExpiryDate,
+// 		d.Reference,
+// 		d.Status,
+// 		d.CreatedAt,
+// 	).Scan(&insertedID)
 
-	return d, nil
-}
+// 	d.Id = insertedID
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-func (r *PaymentRepository) GetSinglePayment(planId string, userId int) (*model.PaymentResponse, error) {
-	query := `
-		SELECT
-			p.id,
-			p.uuid,
-			p.user_id,
-			p.amount_paid,
-			p.plan_id,
-			p.duration,
-			p.expiry_date,
-			p.reference,
-			p.status,
-			p.created_at,
-			p.updated_at,
-			p.deleted_at,
-			u.id AS "user.id",
-			u.uuid AS "user.uuid",
-			u.firstname AS "user.firstname",
-			u.middlename AS "user.middlename",
-			u.lastname AS "user.lastname",
-			u.username AS "user.username",
-			u.email AS "user.email",
-			u.password AS "user.password",
-			u.verified AS "user.verified",
-			u.created_at AS "user.created_at",
-			u.verified_at AS "user.verified_at",
-			u.updated_at AS "user.updated_at",
-			u.deleted_at AS "user.deleted_at",
-			pl.id AS "plan.id",
-			pl.uuid AS "plan.uuid",
-			pl.planname AS "plan.planname",
-			pl.duration AS "plan.duration",
-			pl.price AS "plan.price",
-			pl.details AS "plan.details",
-			pl.status AS "plan.status",
-			pl.created_at AS "plan.created_at",
-			pl.updated_at AS "plan.updated_at",
-			pl.deleted_at AS "plan.deleted_at"
-		FROM
-			payments p
-		JOIN
-			users u ON p.user_id = u.id
-		JOIN
-			plans pl ON p.plan_id = pl.id
-		WHERE
-			p.uuid = $2
-		AND 
-		   u.id = $1	;
-	`
+// 	return d, nil
+// }
 
-	// Prepare the SQL statement
-	stmt, err := r.DB.Prepare(query)
-	if err != nil {
-		return nil, err
-	}
-	defer stmt.Close()
+// func (r *PaymentRepository) GetSinglePayment(planId string, userId int) (*model.PaymentResponse, error) {
+// 	query := `
+// 		SELECT
+// 			p.id,
+// 			p.uuid,
+// 			p.user_id,
+// 			p.amount_paid,
+// 			p.plan_id,
+// 			p.duration,
+// 			p.expiry_date,
+// 			p.reference,
+// 			p.status,
+// 			p.created_at,
+// 			p.updated_at,
+// 			p.deleted_at,
+// 			u.id AS "user.id",
+// 			u.uuid AS "user.uuid",
+// 			u.firstname AS "user.firstname",
+// 			u.middlename AS "user.middlename",
+// 			u.lastname AS "user.lastname",
+// 			u.username AS "user.username",
+// 			u.email AS "user.email",
+// 			u.password AS "user.password",
+// 			u.verified AS "user.verified",
+// 			u.created_at AS "user.created_at",
+// 			u.verified_at AS "user.verified_at",
+// 			u.updated_at AS "user.updated_at",
+// 			u.deleted_at AS "user.deleted_at",
+// 			pl.id AS "plan.id",
+// 			pl.uuid AS "plan.uuid",
+// 			pl.planname AS "plan.planname",
+// 			pl.duration AS "plan.duration",
+// 			pl.price AS "plan.price",
+// 			pl.details AS "plan.details",
+// 			pl.status AS "plan.status",
+// 			pl.created_at AS "plan.created_at",
+// 			pl.updated_at AS "plan.updated_at",
+// 			pl.deleted_at AS "plan.deleted_at"
+// 		FROM
+// 			payments p
+// 		JOIN
+// 			users u ON p.user_id = u.id
+// 		JOIN
+// 			plans pl ON p.plan_id = pl.id
+// 		WHERE
+// 			p.uuid = $2
+// 		AND 
+// 		   u.id = $1	;
+// 	`
 
-	// Execute the prepared statement
+// 	// Prepare the SQL statement
+// 	stmt, err := r.DB.Prepare(query)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	defer stmt.Close()
 
-	row := stmt.QueryRow(userId, planId)
+// 	// Execute the prepared statement
 
-	// Scan the result into a PaymentResponse struct
-	var payment model.PaymentResponse
+// 	row := stmt.QueryRow(userId, planId)
 
-	var updatedAt sql.NullTime
-	var deletedAt sql.NullTime
+// 	// Scan the result into a PaymentResponse struct
+// 	var payment model.PaymentResponse
 
-	var userverified sql.NullTime
-	var userdeleted sql.NullTime
-	var userupdated sql.NullTime
+// 	var updatedAt sql.NullTime
+// 	var deletedAt sql.NullTime
 
-	var planupated sql.NullTime
-	var plandeleted sql.NullTime
+// 	var userverified sql.NullTime
+// 	var userdeleted sql.NullTime
+// 	var userupdated sql.NullTime
 
-	err = row.Scan(
-		&payment.Id,
-		&payment.UUID,
-		&payment.UserId,
-		&payment.AmountPaid,
-		&payment.PlanId,
-		&payment.Duration,
-		&payment.ExpiryDate,
-		&payment.Reference,
-		&payment.Status,
-		&payment.CreatedAt,
-		&updatedAt,
-		&deletedAt,
-		&payment.User.ID,
-		&payment.User.UUID,
-		&payment.User.FirstName,
-		&payment.User.MiddleName,
-		&payment.User.LastName,
-		&payment.User.UserName,
-		&payment.User.Email,
-		&payment.User.Password,
-		&payment.User.Verified,
-		&payment.User.CreatedAt,
-		&userverified,
-		&userupdated,
-		&userdeleted,
-		&payment.Plan.Id,
-		&payment.Plan.UUID,
-		&payment.Plan.PlanName,
-		&payment.Plan.Duration,
-		&payment.Plan.Price,
-		&payment.Plan.Details,
-		&payment.Plan.Status,
-		&payment.Plan.CreatedAt,
-		&planupated,
-		&plandeleted,
-	)
-	if err != nil {
-		return nil, err
-	}
+// 	var planupated sql.NullTime
+// 	var plandeleted sql.NullTime
 
-	// setTime(deletedAt, &payment.UpdatedAt)
+// 	err = row.Scan(
+// 		&payment.Id,
+// 		&payment.UUID,
+// 		&payment.UserId,
+// 		&payment.AmountPaid,
+// 		&payment.PlanId,
+// 		&payment.Duration,
+// 		&payment.ExpiryDate,
+// 		&payment.Reference,
+// 		&payment.Status,
+// 		&payment.CreatedAt,
+// 		&updatedAt,
+// 		&deletedAt,
+// 		&payment.User.ID,
+// 		&payment.User.UUID,
+// 		&payment.User.FirstName,
+// 		&payment.User.MiddleName,
+// 		&payment.User.LastName,
+// 		&payment.User.UserName,
+// 		&payment.User.Email,
+// 		&payment.User.Password,
+// 		&payment.User.Verified,
+// 		&payment.User.CreatedAt,
+// 		&userverified,
+// 		&userupdated,
+// 		&userdeleted,
+// 		&payment.Plan.Id,
+// 		&payment.Plan.UUID,
+// 		&payment.Plan.PlanName,
+// 		&payment.Plan.Duration,
+// 		&payment.Plan.Price,
+// 		&payment.Plan.Details,
+// 		&payment.Plan.Status,
+// 		&payment.Plan.CreatedAt,
+// 		&planupated,
+// 		&plandeleted,
+// 	)
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-	SetTime(updatedAt, &payment.UpdatedAt)
-	SetTime(deletedAt, &payment.DeletedAt)
+// 	// setTime(deletedAt, &payment.UpdatedAt)
 
-	// // Set UpdatedAt based on the presence of updatedAt or deletedAt
-	// if deletedAt.Valid {
-	// 	payment.UpdatedAt = deletedAt.Time.Format(time.RFC3339Nano)
-	// }
-	// if updatedAt.Valid {
-	// 	payment.UpdatedAt = updatedAt.Time.Format(time.RFC3339Nano)
-	// }
+// 	SetTime(updatedAt, &payment.UpdatedAt)
+// 	SetTime(deletedAt, &payment.DeletedAt)
 
-	if userverified.Valid {
-		payment.User.VerifiedAt = userverified.Time.Format(time.RFC3339Nano)
-	}
+// 	// // Set UpdatedAt based on the presence of updatedAt or deletedAt
+// 	// if deletedAt.Valid {
+// 	// 	payment.UpdatedAt = deletedAt.Time.Format(time.RFC3339Nano)
+// 	// }
+// 	// if updatedAt.Valid {
+// 	// 	payment.UpdatedAt = updatedAt.Time.Format(time.RFC3339Nano)
+// 	// }
 
-	if userupdated.Valid {
-		payment.User.UpdatedAt = userupdated.Time.Format(time.RFC3339Nano)
-	}
+// 	if userverified.Valid {
+// 		payment.User.VerifiedAt = userverified.Time.Format(time.RFC3339Nano)
+// 	}
 
-	if userdeleted.Valid {
-		payment.User.DeletedAt = userdeleted.Time.Format(time.RFC3339Nano)
-	}
+// 	if userupdated.Valid {
+// 		payment.User.UpdatedAt = userupdated.Time.Format(time.RFC3339Nano)
+// 	}
 
-	if plandeleted.Valid {
-		payment.Plan.DeletedAt = plandeleted.Time.Format(time.RFC3339Nano)
-	}
+// 	if userdeleted.Valid {
+// 		payment.User.DeletedAt = userdeleted.Time.Format(time.RFC3339Nano)
+// 	}
 
-	if planupated.Valid {
-		payment.Plan.UpdatedAt = plandeleted.Time.Format(time.RFC3339Nano)
-	}
+// 	if plandeleted.Valid {
+// 		payment.Plan.DeletedAt = plandeleted.Time.Format(time.RFC3339Nano)
+// 	}
 
-	return &payment, nil
-}
+// 	if planupated.Valid {
+// 		payment.Plan.UpdatedAt = plandeleted.Time.Format(time.RFC3339Nano)
+// 	}
 
-func (r *PaymentRepository) GetAllPayments(userId int) ([]model.PaymentResponse, error) {
-	query := `SELECT
-			p.id,
-			p.uuid,
-			p.user_id,
-			p.amount_paid,
-			p.plan_id,
-			p.duration,
-			p.expiry_date,
-			p.reference,
-			p.status,
-			p.created_at,
-			p.updated_at,
-			p.deleted_at,
-			u.id AS "user.id",
-			u.uuid AS "user.uuid",
-			u.firstname AS "user.firstname",
-			u.middlename AS "user.middlename",
-			u.lastname AS "user.lastname",
-			u.username AS "user.username",
-			u.email AS "user.email",
-			u.password AS "user.password",
-			u.verified AS "user.verified",
-			u.created_at AS "user.created_at",
-			u.verified_at AS "user.verified_at",
-			u.updated_at AS "user.updated_at",
-			u.deleted_at AS "user.deleted_at",
-			pl.id AS "plan.id",
-			pl.uuid AS "plan.uuid",
-			pl.planname AS "plan.planname",
-			pl.duration AS "plan.duration",
-			pl.price AS "plan.price",
-			pl.details AS "plan.details",
-			pl.status AS "plan.status",
-			pl.created_at AS "plan.created_at",
-			pl.updated_at AS "plan.updated_at",
-			pl.deleted_at AS "plan.deleted_at"
-		FROM
-			payments p
-		JOIN
-			users u ON p.user_id = u.id
-		JOIN
-			plans pl ON p.plan_id = pl.id
+// 	return &payment, nil
+// }
 
-			where u.id = $1
-		`
+// func (r *PaymentRepository) GetAllPayments(userId int) ([]model.PaymentResponse, error) {
+// 	query := `SELECT
+// 			p.id,
+// 			p.uuid,
+// 			p.user_id,
+// 			p.amount_paid,
+// 			p.plan_id,
+// 			p.duration,
+// 			p.expiry_date,
+// 			p.reference,
+// 			p.status,
+// 			p.created_at,
+// 			p.updated_at,
+// 			p.deleted_at,
+// 			u.id AS "user.id",
+// 			u.uuid AS "user.uuid",
+// 			u.firstname AS "user.firstname",
+// 			u.middlename AS "user.middlename",
+// 			u.lastname AS "user.lastname",
+// 			u.username AS "user.username",
+// 			u.email AS "user.email",
+// 			u.password AS "user.password",
+// 			u.verified AS "user.verified",
+// 			u.created_at AS "user.created_at",
+// 			u.verified_at AS "user.verified_at",
+// 			u.updated_at AS "user.updated_at",
+// 			u.deleted_at AS "user.deleted_at",
+// 			pl.id AS "plan.id",
+// 			pl.uuid AS "plan.uuid",
+// 			pl.planname AS "plan.planname",
+// 			pl.duration AS "plan.duration",
+// 			pl.price AS "plan.price",
+// 			pl.details AS "plan.details",
+// 			pl.status AS "plan.status",
+// 			pl.created_at AS "plan.created_at",
+// 			pl.updated_at AS "plan.updated_at",
+// 			pl.deleted_at AS "plan.deleted_at"
+// 		FROM
+// 			payments p
+// 		JOIN
+// 			users u ON p.user_id = u.id
+// 		JOIN
+// 			plans pl ON p.plan_id = pl.id
 
-	// Prepare the SQL statement
-	stmt, err := r.DB.Prepare(query)
-	if err != nil {
-		return nil, err
-	}
-	defer stmt.Close()
+// 			where u.id = $1
+// 		`
 
-	// Execute the prepared statement
-	rows, err := stmt.Query(userId)
-	if err != nil {
-		return nil, err
-	}
-	defer rows.Close()
+// 	// Prepare the SQL statement
+// 	stmt, err := r.DB.Prepare(query)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	defer stmt.Close()
 
-	// Iterate through the result set and map to PaymentResponse struct
-	var payments []model.PaymentResponse
-	for rows.Next() {
-		var payment model.PaymentResponse
-		var updatedAt sql.NullTime
-		var deletedAt sql.NullTime
+// 	// Execute the prepared statement
+// 	rows, err := stmt.Query(userId)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	defer rows.Close()
 
-		var userverified sql.NullTime
-		var userdeleted sql.NullTime
-		var userupdated sql.NullTime
+// 	// Iterate through the result set and map to PaymentResponse struct
+// 	var payments []model.PaymentResponse
+// 	for rows.Next() {
+// 		var payment model.PaymentResponse
+// 		var updatedAt sql.NullTime
+// 		var deletedAt sql.NullTime
 
-		var planupated sql.NullTime
-		var plandeleted sql.NullTime
+// 		var userverified sql.NullTime
+// 		var userdeleted sql.NullTime
+// 		var userupdated sql.NullTime
 
-		err := rows.Scan(
-			&payment.Id,
-			&payment.UUID,
-			&payment.UserId,
-			&payment.AmountPaid,
-			&payment.PlanId,
-			&payment.Duration,
-			&payment.ExpiryDate,
-			&payment.Reference,
-			&payment.Status,
-			&payment.CreatedAt,
-			&updatedAt,
-			&deletedAt,
-			&payment.User.ID,
-			&payment.User.UUID,
-			&payment.User.FirstName,
-			&payment.User.MiddleName,
-			&payment.User.LastName,
-			&payment.User.UserName,
-			&payment.User.Email,
-			&payment.User.Password,
-			&payment.User.Verified,
-			&payment.User.CreatedAt,
-			&userverified,
-			&userupdated,
-			&userdeleted,
-			&payment.Plan.Id,
-			&payment.Plan.UUID,
-			&payment.Plan.PlanName,
-			&payment.Plan.Duration,
-			&payment.Plan.Price,
-			&payment.Plan.Details,
-			&payment.Plan.Status,
-			&payment.Plan.CreatedAt,
-			&planupated,
-			&plandeleted,
-		)
-		if err != nil {
-			return nil, err
-		}
+// 		var planupated sql.NullTime
+// 		var plandeleted sql.NullTime
 
-		if deletedAt.Valid {
-			payment.UpdatedAt = deletedAt.Time.Format(time.RFC3339Nano)
-		}
-		if updatedAt.Valid {
-			payment.UpdatedAt = updatedAt.Time.Format(time.RFC3339Nano)
-		}
+// 		err := rows.Scan(
+// 			&payment.Id,
+// 			&payment.UUID,
+// 			&payment.UserId,
+// 			&payment.AmountPaid,
+// 			&payment.PlanId,
+// 			&payment.Duration,
+// 			&payment.ExpiryDate,
+// 			&payment.Reference,
+// 			&payment.Status,
+// 			&payment.CreatedAt,
+// 			&updatedAt,
+// 			&deletedAt,
+// 			&payment.User.ID,
+// 			&payment.User.UUID,
+// 			&payment.User.FirstName,
+// 			&payment.User.MiddleName,
+// 			&payment.User.LastName,
+// 			&payment.User.UserName,
+// 			&payment.User.Email,
+// 			&payment.User.Password,
+// 			&payment.User.Verified,
+// 			&payment.User.CreatedAt,
+// 			&userverified,
+// 			&userupdated,
+// 			&userdeleted,
+// 			&payment.Plan.Id,
+// 			&payment.Plan.UUID,
+// 			&payment.Plan.PlanName,
+// 			&payment.Plan.Duration,
+// 			&payment.Plan.Price,
+// 			&payment.Plan.Details,
+// 			&payment.Plan.Status,
+// 			&payment.Plan.CreatedAt,
+// 			&planupated,
+// 			&plandeleted,
+// 		)
+// 		if err != nil {
+// 			return nil, err
+// 		}
 
-		if userverified.Valid {
-			payment.User.VerifiedAt = userverified.Time.Format(time.RFC3339Nano)
-		}
+// 		if deletedAt.Valid {
+// 			payment.UpdatedAt = deletedAt.Time.Format(time.RFC3339Nano)
+// 		}
+// 		if updatedAt.Valid {
+// 			payment.UpdatedAt = updatedAt.Time.Format(time.RFC3339Nano)
+// 		}
 
-		if userupdated.Valid {
-			payment.User.UpdatedAt = userupdated.Time.Format(time.RFC3339Nano)
-		}
+// 		if userverified.Valid {
+// 			payment.User.VerifiedAt = userverified.Time.Format(time.RFC3339Nano)
+// 		}
 
-		if userdeleted.Valid {
-			payment.User.DeletedAt = userdeleted.Time.Format(time.RFC3339Nano)
-		}
+// 		if userupdated.Valid {
+// 			payment.User.UpdatedAt = userupdated.Time.Format(time.RFC3339Nano)
+// 		}
 
-		if plandeleted.Valid {
-			payment.Plan.DeletedAt = plandeleted.Time.Format(time.RFC3339Nano)
-		}
+// 		if userdeleted.Valid {
+// 			payment.User.DeletedAt = userdeleted.Time.Format(time.RFC3339Nano)
+// 		}
 
-		if planupated.Valid {
-			payment.Plan.UpdatedAt = plandeleted.Time.Format(time.RFC3339Nano)
-		}
+// 		if plandeleted.Valid {
+// 			payment.Plan.DeletedAt = plandeleted.Time.Format(time.RFC3339Nano)
+// 		}
 
-		payments = append(payments, payment)
-	}
+// 		if planupated.Valid {
+// 			payment.Plan.UpdatedAt = plandeleted.Time.Format(time.RFC3339Nano)
+// 		}
 
-	return payments, nil
-}
+// 		payments = append(payments, payment)
+// 	}
+
+// 	return payments, nil
+// }
