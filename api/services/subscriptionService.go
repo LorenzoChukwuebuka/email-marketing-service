45938e4cabb0687adf05dde4d660d13947ad4542
LorenzoChukwuebuka <lawrenceobi2@gmail.com>
2023-12-04 22:10:49 +0100

    subscription

--- api/services/subscriptionService.go
+++ api/services/subscriptionService.go
@@ -3,10 +3,22 @@ package services
 import (
 	"email-marketing-service/api/model"
 	"email-marketing-service/api/repository"
+	"time"
 )
 
 type SubscriptionService struct {
 	SubscriptionRepo *repository.SubscriptionRepository
+}
+
+type CurrentSubscription struct {
+	ID        int
+	UUID      string
+	UserID    int
+	PlanID    int
+	PaymentID int
+	StartDate time.Time
+	EndDate   time.Time
+	Expired   bool
 }
 
 func NewSubscriptionService(subscriptionRepo *repository.SubscriptionRepository) *SubscriptionService {
@@ -21,8 +33,14 @@ func (s *SubscriptionService) CreateSubscription(d *model.SubscriptionModel) (*m
 	return d, nil
 }
 
-func (s *SubscriptionService) GetCurrentRunningSubscription(subscriptionId string) {
-   
+func (s *SubscriptionService) GetAllCurrentRunningSubscription(subscriptionId string) ([]model.SubscriptionModel, error) {
+	subscriptions, err := s.SubscriptionRepo.GetAllSubscriptions(subscriptionId)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return subscriptions, err
 }
 
 /*
