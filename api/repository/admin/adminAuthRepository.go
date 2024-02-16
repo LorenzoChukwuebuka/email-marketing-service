1504dc68fe81ee7d5269220014796d3d26b8814e
LorenzoChukwuebuka <lawrenceobi2@gmail.com>
2024-02-16 14:13:32 +0100

    refactored the dependency with autowiring

--- api/repository/admin/adminAuthRepository.go
+++ api/repository/admin/adminAuthRepository.go
@@ -1,10 +1,8 @@
 package adminrepository
 
 import (
-	 
 	adminmodel "email-marketing-service/api/model/admin"
-	 
-
+	"fmt"
 	"gorm.io/gorm"
 )
 
@@ -17,14 +15,15 @@ func NewAdminRepository(db *gorm.DB) *AdminRepository {
 }
 
 func (r *AdminRepository) CreateAdmin(d *adminmodel.Admin) (*adminmodel.Admin, error) {
-	
+
+	if err := r.DB.Create(&d).Error; err != nil {
+		return nil, fmt.Errorf("failed to insert user: %w", err)
+	}
 
 	return d, nil
 }
 
 func (r *AdminRepository) Login(d *adminmodel.AdminLogin) (*adminmodel.AdminResponse, error) {
-
-	
 
 	return nil, nil
 
