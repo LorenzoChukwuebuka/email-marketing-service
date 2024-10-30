39dc138c3cfd0a1c74805d92d66def5feb7164d9
LorenzoChukwuebuka <lawrenceobi2@gmail.com>
2024-10-30 16:35:05 +0100

    relay modified

--- backend/smtp/smtp_config.go
+++ backend/smtp/smtp_config.go
@@ -1,20 +1,24 @@
 package smtp_server 
 
-
-// var config = &RateLimiterConfig{
-//     ConnectionsPerIP:   50,                 // 50 connections
-//     ConnectionWindow:   30 * time.Minute,   // per 30 minutes
-//     MessagesPerIP:     500,                // 500 messages
-//     MessageWindow:     time.Hour,          // per hour
-//     RecipientsPerMsg:  100,               // 100 recipients per message
-//     DefaultMonthQuota: 50000,             // 50,000 messages per month
-// }
+import (
+	"time"
+)
 
 
-// config := &RelayConfig{
-//     Debug:          true,
-//     DialTimeout:    15 * time.Second,
-//     RetryAttempts:  5,
-//     RetryDelay:     3 * time.Second,
-//     PreferredPorts: []string{"587", "465"},  // Skip port 25 if not needed
-// }
+var RLconfig = &RateLimiterConfig{
+    ConnectionsPerIP:   50,                 // 50 connections
+    ConnectionWindow:   30 * time.Minute,   // per 30 minutes
+    MessagesPerIP:     500,                // 500 messages
+    MessageWindow:     time.Hour,          // per hour
+    RecipientsPerMsg:  100,               // 100 recipients per message
+    DefaultMonthQuota: 50000,             // 50,000 messages per month
+}
+
+
+var RCconfig = &RelayConfig{
+    Debug:          true,
+    DialTimeout:    15 * time.Second,
+    RetryAttempts:  5,
+    RetryDelay:     3 * time.Second,
+    PreferredPorts: []string{"587", "465"},  // Skip port 25 if not needed
+}
